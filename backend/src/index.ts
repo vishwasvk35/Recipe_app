cb17a546d87c1cc755fff8f2f3898fdd7e655452
vishwasvk35 <vishwasvk35@gmail.com>
2024-12-16 20:05:48 +0530

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors'
+
+const app = express();
+
+app.use(express.json()); //convert the body of request and response into json
+app.use(cors()); //used to avoid security related errors
+
+app.get('/', (req, res) => {
+    res.send('Server is working!');
+}); //test code to check if everything is working properly
+
+app.listen(5000, ()=>{
+    console.log("running on port: 5000");
+})
